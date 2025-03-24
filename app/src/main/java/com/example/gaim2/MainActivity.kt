3d03a30a2c0833e3a91c5c4af0ba7d3ad9249882
braunc8 <chloecbraun@gmail.com>
2025-03-24 15:03:51 -0400

    added files

--- app/src/main/java/com/example/gaim2/MainActivity.kt
+++ app/src/main/java/com/example/gaim2/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.gaim2
 
 import android.os.Bundle
+import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.gaim2.ui.theme.Gaim2Theme
 
@@ -17,9 +21,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Gaim2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        setContent { //calls setContent with expression inside brackets as content parameter (because it is the last parameter)
+            Gaim2Theme { //calls Gaim2Theme with expression inside brackets as content parameter (because it is the last parameter)
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding -> //calls Scaffold with modifier in parameters and innerPadding -> XYZ as content parameter
+                    //inner padding type is inferred from Scaffold definition
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -28,14 +33,18 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Cyan) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
