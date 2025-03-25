af7d52a8d5ae63d4a11f345f4650723d4af3269f
braunc8 <chloecbraun@gmail.com>
2025-03-24 23:05:01 -0400

    Added account settings frontend skeleton

--- app/src/main/java/com/example/gaim2/ui/page/account/AccountSettingsPage.kt
+++ app/src/main/java/com/example/gaim2/ui/page/account/AccountSettingsPage.kt
@@ -4,5 +4,5 @@ import com.example.gaim2.ui.page.ToolbarPage
 
 //has buttons to go to account history, and reset password and login, for now i was thinking reset password and login
 //could be like popup windows but whatever is easier is fine
-class AccountPage : ToolbarPage () {
+class AccountSettingsPage : ToolbarPage () {
 }
