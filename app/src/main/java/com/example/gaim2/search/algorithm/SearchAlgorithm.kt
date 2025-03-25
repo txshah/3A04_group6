e7aa7adfc28bf83485ffdad0f6742c502253bae1
braunc8 <chloecbraun@gmail.com>
2025-03-24 20:28:57 -0400

    fixed some minor walking skeleton errors

--- app/src/main/java/com/example/gaim2/search/algorithm/SearchAlgorithm.kt
+++ app/src/main/java/com/example/gaim2/search/algorithm/SearchAlgorithm.kt
@@ -1,12 +1,8 @@
 package com.example.gaim2.search.algorithm
 
+import com.example.gaim2.search.SearchResult
+
 //receives relevant input type and returns a SEARCH RESULT based on input received
 interface SearchAlgorithm <InputType>{
     fun search(input: InputType): SearchResult;
 }
-
-interface Input<out T>{
-
-}
-
-class SearchResult
