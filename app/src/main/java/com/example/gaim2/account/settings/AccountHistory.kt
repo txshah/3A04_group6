b7c8d393af110fcabbc8bdb7247dcb271984e603
braunc8 <chloecbraun@gmail.com>
2025-03-24 23:04:38 -0400

    Added account settings backend skeletons

---
+++ app/src/main/java/com/example/gaim2/account/settings/AccountHistory.kt
@@ -0,0 +1,5 @@
+package com.example.gaim2.account.settings
+
+//Manages account history UI and interactions
+class AccountHistory {
+}
---
+++ app/src/main/java/com/example/gaim2/account/settings/AccountSettingsManager.kt
@@ -0,0 +1,7 @@
+package com.example.gaim2.account.settings
+
+//runs interactions between the UI and the various needed pages, ex. triggers account history page, allows user
+//to reset password and username
+class AccountSettingsManager {
+
+}
