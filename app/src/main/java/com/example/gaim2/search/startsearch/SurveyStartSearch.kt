e611efa52fb3b38b6f6fb9cd5fadd4047dbca0e6
Tvesha Shah <tveshashah13@gmail.com>
2025-03-24 20:44:11 -0400

    db

--- app/src/main/java/com/example/gaim2/search/startsearch/SurveyStartSearch.kt
+++ app/src/main/java/com/example/gaim2/search/startsearch/SurveyStartSearch.kt
@@ -1,6 +1,10 @@
 package com.example.gaim2.search.startsearch
 
-//see StartSearch for description, Survey based input
-class SurveyStartSearch : StartSearch {
+import com.example.gaim2.search.algorithm.SearchResult
 
+//see StartSearch for description, Survey based input
+class SurveyStartSearch<String> : StartSearch<String>() {
+    override fun someMethod(param: String): String {
+        TODO("Implement survey-based logic")
+    }
 }
