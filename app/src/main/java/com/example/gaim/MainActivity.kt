83a1105efb69b2918a958d533cb474dbffad91e7
braunc8 <chloecbraun@gmail.com>
2025-03-28 17:19:50 -0400

    Added set up of search buttons to home page

--- app/src/main/java/com/example/gaim/MainActivity.kt
+++ app/src/main/java/com/example/gaim/MainActivity.kt
@@ -7,9 +7,18 @@ import android.widget.ImageButton
 
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.gaim.search.Opposable
+import com.example.gaim.search.SearchActivity
+import com.example.gaim.search.SearchState
 
 //Homepage
 class MainActivity : AppCompatActivity () {
+    //hash map containing relevant search button activities to their button IDs
+    private val searchButtonMap: Map<SearchActivity, Int> = hashMapOf(
+        SearchActivity.SURVEY to R.id.survey_search,
+        SearchActivity.FREEFORM to R.id.freeform_search,
+        SearchActivity.IMAGE to R.id.image_search
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,19 +32,56 @@ class MainActivity : AppCompatActivity () {
         val homeButton = findViewById<ImageButton>(R.id.home_page)
         val accountButton = findViewById<ImageButton>(R.id.account_page)
 
-        //search buttons
-        val surveySearchButton = findViewById<Button>(R.id.survey_search)
-        val freeformSearchButton = findViewById<Button>(R.id.freeform_search)
-        val imageSearchButton = findViewById<Button>(R.id.image_search)
+        //set up search buttons
+        val searchStateTrackers = setUpSearchButtons()
 
         //start button
         val startSearchButton = findViewById<Button>(R.id.start_search)
 
-        //sample button listener
-        homeButton.setOnClickListener {
-            Log.d("BUTTON", "Hello") // Prints "Hello" to logcat
-            println("Hello") // Prints "Hello" to the console (debugging)
+        startSearchButton.setOnClickListener {
+            runSearch(searchStateTrackers)
+        }
+    }
+
+    private fun setUpSearchButtons(): Collection<SearchStateTracker>{
+        var searchStateTrackers: MutableCollection<SearchStateTracker> = LinkedHashSet()
+
+        for (entry in searchButtonMap){
+            val searchButton = findViewById<Button>(entry.value)
+            searchStateTrackers.add(SearchStateTracker(entry.key, searchButton))
+
         }
 
+        return searchStateTrackers
+    }
+
+    private fun runSearch(searchStateTrackers: Collection<SearchStateTracker>){
+        for(tracker in searchStateTrackers){
+            intent.putExtra(tracker.searchName(), tracker.state())
+        }
+    }
+}
+
+//Creates a tracker for a search state and associates a search activity
+class SearchStateTracker(private val search: SearchActivity, button: Button) : SwitchButtonTracker<SearchState>(button) {
+    override var state: SearchState = SearchState.PROCESSED;
+
+    fun searchName(): String{
+        return search.name
+    }
+}
+
+//creates a switch button that tracks the button state via an opposable class (enum)
+abstract class SwitchButtonTracker<K : Opposable<K>>(button: Button){
+    protected abstract var state: K;
+
+    init{
+        button.setOnClickListener {
+            state = state.opposite()
+        }
+    }
+
+    fun state(): Boolean{ //makes state read only (val vs var)
+        return state.value
     }
 }
--- app/src/main/java/com/example/gaim/search/SearchActivity.kt
+++ app/src/main/java/com/example/gaim/search/SearchActivity.kt
@@ -3,7 +3,11 @@ package com.example.gaim.search
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gaim.ui.search.*
 
-enum class SearchActivity (public val key: String, public val search: Class<out AppCompatActivity>) {
+interface Named {
+    val value: String
+}
+
+enum class SearchActivity (override val value: String, val search: Class<out AppCompatActivity>): Named{
     SURVEY("survey", SurveySearchActivity::class.java),
     FREEFORM("freeform", FreeformSearchActivity::class.java),
     IMAGE("image", ImageSearchActivity::class.java);
@@ -16,8 +20,24 @@ enum class SearchActivity (public val key: String, public val search: Class<out 
     }
 }
 
+interface Opposable<T>{
+    val value: Boolean
+    fun opposite(): T
+}
+
 //pending indicates the search must be run, processed means that the search must no longer be run
-enum class SearchState (public val value: Boolean){
+enum class SearchState (override val value: Boolean) : Opposable<SearchState>{
     PENDING(true),
     PROCESSED(false);
+
+    companion object {
+        fun find(value: Boolean): SearchState? {
+            val state = SearchState.entries.find { it.value == value };
+            return state
+        }
+    }
+
+    override fun opposite(): SearchState {
+        return find(!this.value) !!
+    }
 }
