fa628c0c0f96cb04160b073bb5ad736ffe01e090
braunc8 <chloecbraun@gmail.com>
2025-03-28 10:41:32 -0400

    added basic login page

--- app/src/main/java/com/example/gaim/LoginActivity.kt
+++ app/src/main/java/com/example/gaim/LoginActivity.kt
@@ -2,10 +2,15 @@ package com.example.gaim
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.enableEdgeToEdge
 
 class LoginActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TODO("Not yet implemented")
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_login)
+
+
     }
 }
