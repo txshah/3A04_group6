1be318a893d54d2edf9f652cc40a524232ea0bf4
braunc8 <chloecbraun@gmail.com>
2025-03-31 15:09:17 -0400

    Added search activity interface updates

--- app/src/main/java/com/example/gaim/ui/search/AbstractSearchActivity.kt
+++ app/src/main/java/com/example/gaim/ui/search/AbstractSearchActivity.kt
@@ -2,13 +2,15 @@ package com.example.gaim.ui.search
 
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gaim.search.SearchController
+import com.example.gaim.search.algorithm.SearchAlgorithm
 
-abstract class AbstractSearchActivity: AppCompatActivity ()  {
+abstract class AbstractSearchActivity <InputType>: AppCompatActivity ()  {
+    protected abstract val algorithm: SearchAlgorithm<InputType>;
 
     //run this function when the freeform search has all
     //required information and the page can move to the
     // next search type pass in "this" in inheritor classes
-    protected fun completeSearch(inheritor: AbstractSearchActivity){
+    protected fun completeSearch(inheritor: AbstractSearchActivity<InputType>){
         val searchController = SearchController(inheritor, intent)
         searchController.complete(inheritor.javaClass)
     }
--- app/src/main/java/com/example/gaim/ui/search/FreeformSearchActivity.kt
+++ app/src/main/java/com/example/gaim/ui/search/FreeformSearchActivity.kt
@@ -1,10 +1,24 @@
 package com.example.gaim.ui.search
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.gaim.R
 
 class FreeformSearchActivity: AbstractSearchActivity () {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.search)
+
+        val textInput = findViewById<EditText>(R.id.input_text)
+
+        val submitFreeform = findViewById<Button>(R.id.submit_freeform)
+
+        submitFreeform.setOnClickListener {
+            textInput.text
+        }
         TODO("Not yet implemented")
     }
 
