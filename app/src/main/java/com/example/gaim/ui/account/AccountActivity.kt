0b4edb27c306e1ea6f709b791fcdbb17cff1397a
braunc8 <chloecbraun@gmail.com>
2025-04-06 18:52:37 -0400

    Displays username on account id

--- app/src/main/java/com/example/gaim/ui/account/AccountActivity.kt
+++ app/src/main/java/com/example/gaim/ui/account/AccountActivity.kt
@@ -1,17 +1,25 @@
 package com.example.gaim.ui.account
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import com.example.gaim.R
 import com.example.gaim.ui.AbstractActivity
+import org.w3c.dom.Text
 
 class AccountActivity : AbstractActivity() {
+    private val usernameID = R.id.user_name
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.savedaccount)
 
         val username = intent.getStringExtra(Login.USERNAME.value)
-        val password = intent.getStringExtra(Login.PASSWORD.value)
+
+        val usernameID = findViewById<TextView>(usernameID)
+
+        usernameID.text = username
 
     }
 
