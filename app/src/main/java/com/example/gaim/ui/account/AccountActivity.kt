b4d2387753bba8e9b145ab0eef9e7669766b822d
braunc8 <chloecbraun@gmail.com>
2025-04-06 20:00:41 -0400

    Added home button to account settings

--- app/src/main/java/com/example/gaim/ui/account/AccountActivity.kt
+++ app/src/main/java/com/example/gaim/ui/account/AccountActivity.kt
@@ -1,7 +1,6 @@
 package com.example.gaim.ui.account
 
 import android.os.Bundle
-import android.util.Log
 
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -10,9 +9,11 @@ import com.example.gaim.ui.AbstractActivity
 import com.example.gaim.ui.utility.CustomAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gaim.account.settings.AccountSettings
+import com.example.gaim.ui.MainpageActivity
 
 class AccountActivity : AbstractActivity() {
     private val usernameID = R.id.user_name
+    private val homeID = R.id.homepage2
     private lateinit var accountSettings: AccountSettings
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,16 +25,31 @@ class AccountActivity : AbstractActivity() {
         val password = intent.getStringExtra(Login.PASSWORD.value)
         accountSettings = AccountSettings(this, username.toString(), password.toString())
 
+        setUsername(username.toString())
+        setHome()
+        showPreviousResults()
+    }
+
+    //sets topbar val to current user
+    private fun setUsername(username: String){
         //setting the top value to a username
         val usernameID = findViewById<TextView>(usernameID)
         usernameID.text = username
+    }
 
+    //sets up the home button
+    private fun setHome(){
+        val homeID = findViewById<TextView>(homeID)
+
+        homeID.setOnClickListener {
+            nextActivity(MainpageActivity.MAIN, intent)
+        }
+    }
+
+    //sets up previous results in recycler frame
+    private fun showPreviousResults(){
         //setting up previous user data
         val previousResults =  accountSettings.getAnimals()
-
-        for (result in previousResults){
-            Log.d("TAG", result.toString())
-        }
 
         //adding previous user data to custom adapter
         val customAdapter = CustomAdapter(previousResults.toTypedArray())
@@ -42,5 +58,4 @@ class AccountActivity : AbstractActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = customAdapter
     }
-
 }
